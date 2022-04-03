b4521ec857d408fba0c81fe4581dc0fc6ff69593
JiggyboyK <korede305@gmail.com>
2022-04-03 21:25:30 +0100

    checkpoint

---
+++ Cart.js
@@ -0,0 +1,10 @@
+const addToCart = document.querySelector('h3');
+console.log(addToCart);
+
+// addToCart.addEventListener('click', )
+
+// const addToCart = document.querySelectorAll("h3");
+
+ 
+// console.log(addToCart[0]);
+// console.log(addToCart);
